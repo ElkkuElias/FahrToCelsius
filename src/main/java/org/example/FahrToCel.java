74760d49fe8da495fb2e6074b9e428804f5f2bb0
ElkkuElias <elias.vuorinenkso@gmail.com>
2024-02-16 13:30:19 +0200

    kel

--- src/main/java/org/example/FahrToCel.java
+++ src/main/java/org/example/FahrToCel.java
@@ -7,6 +7,10 @@ public class FahrToCel{
         float c = (float) ((f-32) * 5.0/9.0);
         return Math.round(c);
     }
-
+    public static float kelvintoCel(float kel){
+        return  (kel-273.15F);
     }
+
+
+}
 
--- src/test/java/org/example/FahrToCelTest.java
+++ src/test/java/org/example/FahrToCelTest.java
@@ -11,4 +11,9 @@ class FahrToCelTest {
         FahrToCel Converter = new FahrToCel();
         assertEquals(0,Converter.convertFarh(32));
     }
+    @Test
+    void KelvinToCelsiusTest(){
+        FahrToCel Converter = new FahrToCel();
+        assertEquals(0,Converter.kelvintoCel(273.15F));
+    }
 }
